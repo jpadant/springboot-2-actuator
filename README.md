8df4dec2e1b84b371b1d4b0c06b87a366822437f
Adant Jean Paul <jeanpaul.adant@orange.com>
2019-05-14 23:41:41 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # springboot-2-actuator
+
+# Requests
+
+curl http://localhost:8080/actuator/health
+
+curl http://localhost:8080/actuator/info
---
+++ src/main/java/jpa/example/sb2/api/HelloController.java
@@ -0,0 +1,14 @@
+package jpa.example.sb2.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello from polo";
+    }
+
+}
